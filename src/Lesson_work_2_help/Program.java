aa7c80d92a3df4a5b2f47228d3915449a1c5ebfa
Stanislav <boykostas@gmail.com>
2023-09-01 11:55:22 +0300

    Homework #2 Done

--- src/Lesson_work_2_help/Program.java
+++ src/Lesson_work_2_help/Program.java
@@ -1,2 +1,31 @@
-package Lesson_work_2_help;public class Program {
+package Lesson_work_2_help;
+public class Program {
+    public static void main(String[] args) {
+
+        // Создаем котов
+        Cat[] cats = {
+                new Cat("Барсик", 10),
+                new Cat("Мурзик", 15),
+                new Cat("Персик", 12),
+                new Cat("Борис", 14)
+        };
+
+        // Создаем тарелку и наполняем ее едой
+        Plate plate = new Plate();
+        plate.putFood(35);
+
+        System.out.println("***");
+
+        // Кормим котов
+        for (Cat c : cats) {
+            c.eat(plate);
+        }
+
+        System.out.println("***");
+
+        // Результаты кормления
+        for (Cat c : cats) {
+            System.out.println("Кот " + c.getName() + " сытость: " + c.isSatiety());
+        }
+    }
 }
