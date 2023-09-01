39f78faf25545b49d7d7ac68404dd55fbf7ff39b
Stanislav <boykostas@gmail.com>
2023-09-01 11:54:36 +0300

    HW2 edit file

--- src/Lesson_work_2/Program.java
+++ src/Lesson_work_2/Program.java
@@ -3,6 +3,6 @@ package Lesson_work_2;
 public class Program {
 
     public static void main(String[] args) {
-
+        System.out.println("Привет!");
     }
 }
---
+++ src/Lesson_work_2_help/Plate.java
@@ -0,0 +1,2 @@
+package Lesson_work_2_help;public class Plate {
+}
